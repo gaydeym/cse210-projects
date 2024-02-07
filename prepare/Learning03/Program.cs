522db919bded4a276f27db5a42f5935267d4b53b
gaydeym <gay20001@byui.com>
2024-02-07 20:55:08 +1300

    W03 Prepare

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction value1 = new Fraction();
+        Console.WriteLine(value1.GetFractionString());
+        Console.WriteLine(value1.GetDecimalValue());
+
+        Fraction value2 = new Fraction(5);
+        Console.WriteLine(value2.GetFractionString());
+        Console.WriteLine(value2.GetDecimalValue());
+
+        Fraction value3 = new Fraction(3, 4);
+        Console.WriteLine(value3.GetFractionString());
+        Console.WriteLine(value3.GetDecimalValue());
+
+        Fraction value4 = new Fraction(1, 3);
+        Console.WriteLine(value4.GetFractionString());
+        Console.WriteLine(value4.GetDecimalValue());
     }
 }
